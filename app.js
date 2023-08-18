e9f8a83d2ce21bfa4773b1fbc6f7387f7c8c667e
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2023-08-17 17:45:10 -0700

    third page created

--- app.js
+++ app.js
@@ -27,3 +27,35 @@ clock();
 setInterval(() => {
     clock();
 }, 1000)
+
+
+//second clock
+//timer
+const endDate1= "31 August 2023 12:00 AM";
+const inputs1 = document.querySelectorAll("input[type=number]");
+
+const clock1 = () =>{
+    const end1 = new Date(endDate1);
+    const now1 = new Date();
+    const diff1 = (end1 - now1)/1000;
+
+    if(diff1 < 0) return;
+
+    //convert ito days
+    inputs1[0].value = Math.floor(diff1 / 3600 / 24);
+
+    //convert into hours
+    inputs1[1].value = Math.floor(diff1 / 3600 % 24);
+
+    //convert into minutes
+    inputs1[2].value = Math.floor(diff1 / 60 % 60);
+
+    //convert into seconds
+    inputs1[3].value = Math.floor(diff1 % 60);
+}
+
+clock1();
+
+setInterval(() => {
+    clock1();
+}, 1000)
